eeae0bce74232533a2b9bcc70e2c720721f7b047
Will Drougas <wdrougas@gmail.com>
2019-09-16 17:56:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   add_element_to_start_of_array = ["Adam"]
-  add_element_to_start_of_array.unshift("Sammy")
+  add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
