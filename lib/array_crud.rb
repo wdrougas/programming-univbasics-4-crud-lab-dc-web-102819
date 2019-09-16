9f5d52710a56e4fc43ec25e41930210874026206
Will Drougas <wdrougas@gmail.com>
2019-09-16 17:58:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  remove_element_from_start_of_array = []
+  remove_element_from_start_of_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
