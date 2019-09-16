10c9c8ed2718bc384fc46a2ddc426241d68cc88e
Will Drougas <wdrougas@gmail.com>
2019-09-16 17:59:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   retrieve_element_from_index = ["wow", "I", "am", "really", "learning", "arrays!"]
-  
+  retrieve_element_from_index
 end
 
 def retrieve_first_element_from_array(array)
