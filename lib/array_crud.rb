ab1abc7ea721d1f0e4350d522a1c4f664959bf12
Will Drougas <wdrougas@gmail.com>
2019-09-16 18:03:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,5 +43,5 @@ end
 
 def update_element_from_index(array, index_number, element)
   update_element_from_index = ["wow", "I", "am", "really", "learning", "arrays!"]
-  update_element_from_index[4]
+  update_element_from_index[4] = ''
 end
