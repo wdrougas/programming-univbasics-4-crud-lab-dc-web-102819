20dd0f0e2e7ef6e1e7c65b5b1e9262e5823c1d32
Will Drougas <wdrougas@gmail.com>
2019-09-16 18:02:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  update_element_from_index = []
+  update_element_from_index = ["wow", "I", "am", "really", "learning", "arrays!"]
+  update_element_from_index
 end
