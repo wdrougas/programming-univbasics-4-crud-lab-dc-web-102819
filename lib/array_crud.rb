cb2ffa5435bed7012e151d7a2f419ba684846457
Will Drougas <wdrougas@gmail.com>
2019-09-16 17:54:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Junior", "Billiam", "Sonny", "Kiddo"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array.push("Billy")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array.unshift("Sammy")
 end
 
 def remove_element_from_end_of_array(array)
