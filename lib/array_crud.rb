188690386187a33e27e82f9e5a9402d46f266d6f
Will Drougas <wdrougas@gmail.com>
2019-09-16 18:00:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,11 +32,12 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  retrieve_first_element_from_array = []
+  retrieve_first_element_from_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array = []
 end
 
 def update_element_from_index(array, index_number, element)
