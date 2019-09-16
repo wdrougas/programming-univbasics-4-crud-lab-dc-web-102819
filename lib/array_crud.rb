b4170d8e04aa679b4bc0736c33174e3971491ea5
Will Drougas <wdrougas@gmail.com>
2019-09-16 17:55:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array = ["Will"]
-  add_element_to_end_of_array.push("Billy")
+  add_element_to_end_of_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
